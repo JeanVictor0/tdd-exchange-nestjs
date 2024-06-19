4665b0cf8252a811374b1c13bfd357dae534d86e
JeanVictor0 <jeanvdsroque@gmail.com>
2024-06-18 22:43:13 -0300

    test: convert amount if called with wrong params

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ExchangeModule } from './exchange/exchange.module';
 
 @Module({
-  imports: [ExchangeModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ExchangeService {}
+export class ExchangeService {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async convertAmount(arg0: { form: string; to: string, amount: number }): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+}
